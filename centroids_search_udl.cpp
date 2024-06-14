e894f9f968fcd471501c32c98a063ec2eb8ee080
aliciayuting <yy354@cornell.edu>
2024-06-14 18:45:55 +0000

    incorperated FAISS code

--- centroids_search_udl.cpp
+++ centroids_search_udl.cpp
@@ -1,5 +1,14 @@
 #include <cascade/user_defined_logic_interface.hpp>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
+#include <random>
+
+#include <faiss/IndexFlat.h>
+#include <faiss/gpu/GpuIndexFlat.h>
+#include <faiss/gpu/GpuIndexIVFFlat.h>
+#include <faiss/gpu/StandardGpuResources.h>
 
 namespace derecho{
 namespace cascade{
@@ -16,6 +25,116 @@ std::string get_description() {
 }
 
 class CentroidsSearchOCDPO: public OffCriticalDataPathObserver {
+
+    /***
+    * Function copied from FAISS repository example: 
+    * https://github.com/facebookresearch/faiss/blob/main/tutorial/cpp/4-GPU.cpp
+    ***/
+    static int faiss_gpu_search(){
+        int d = 64;      // dimension
+        int nb = 100000; // database size
+        int nq = 10000;  // nb of queries
+
+        std::mt19937 rng;
+        std::uniform_real_distribution<> distrib;
+
+        float* xb = new float[d * nb];
+        float* xq = new float[d * nq];
+
+        for (int i = 0; i < nb; i++) {
+            for (int j = 0; j < d; j++)
+                xb[d * i + j] = distrib(rng);
+            xb[d * i] += i / 1000.;
+        }
+
+        for (int i = 0; i < nq; i++) {
+            for (int j = 0; j < d; j++)
+                xq[d * i + j] = distrib(rng);
+            xq[d * i] += i / 1000.;
+        }
+
+        faiss::gpu::StandardGpuResources res;
+
+        // Using a flat index
+
+        faiss::gpu::GpuIndexFlatL2 index_flat(&res, d);
+
+        printf("is_trained = %s\n", index_flat.is_trained ? "true" : "false");
+        index_flat.add(nb, xb); // add vectors to the index
+        printf("ntotal = %ld\n", index_flat.ntotal);
+
+        int k = 4;
+
+        { // search xq
+            long* I = new long[k * nq];
+            float* D = new float[k * nq];
+
+            index_flat.search(nq, xq, k, D, I);
+
+            // print results
+            printf("I (5 first results)=\n");
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < k; j++)
+                    printf("%5ld ", I[i * k + j]);
+                printf("\n");
+            }
+
+            printf("I (5 last results)=\n");
+            for (int i = nq - 5; i < nq; i++) {
+                for (int j = 0; j < k; j++)
+                    printf("%5ld ", I[i * k + j]);
+                printf("\n");
+            }
+
+            delete[] I;
+            delete[] D;
+        }
+
+        // Using an IVF index
+
+        int nlist = 100;
+        faiss::gpu::GpuIndexIVFFlat index_ivf(&res, d, nlist, faiss::METRIC_L2);
+
+        assert(!index_ivf.is_trained);
+        index_ivf.train(nb, xb);
+        assert(index_ivf.is_trained);
+        index_ivf.add(nb, xb); // add vectors to the index
+
+        printf("is_trained = %s\n", index_ivf.is_trained ? "true" : "false");
+        printf("ntotal = %ld\n", index_ivf.ntotal);
+
+        { // search xq
+            long* I = new long[k * nq];
+            float* D = new float[k * nq];
+
+            index_ivf.search(nq, xq, k, D, I);
+
+            // print results
+            printf("I (5 first results)=\n");
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < k; j++)
+                    printf("%5ld ", I[i * k + j]);
+                printf("\n");
+            }
+
+            printf("I (5 last results)=\n");
+            for (int i = nq - 5; i < nq; i++) {
+                for (int j = 0; j < k; j++)
+                    printf("%5ld ", I[i * k + j]);
+                printf("\n");
+            }
+
+            delete[] I;
+            delete[] D;
+        }
+
+        delete[] xb;
+        delete[] xq;
+
+        return 0;
+
+    }
+
     virtual void operator () (const derecho::node_id_t sender,
                               const std::string& key_string,
                               const uint32_t prefix_length,
@@ -26,6 +145,7 @@ class CentroidsSearchOCDPO: public OffCriticalDataPathObserver {
                               uint32_t worker_id) override {
         std::cout << "[centroids search ocdpo]: I(" << worker_id << ") received an object from sender:" << sender << " with key=" << key_string 
                   << ", matching prefix=" << key_string.substr(0,prefix_length) << std::endl;
+        faiss_gpu_search();
     }
 
     static std::shared_ptr<OffCriticalDataPathObserver> ocdpo_ptr;
