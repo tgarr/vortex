140b51e0de7b70b278d7852107cc4ad4ab48155e
Jeffrey Qian <jeffreyqdd@gmail.com>
2024-11-07 16:34:05 +0000

    delete download scripts

--- setup/perf_data/download_gist.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-wget -O gist.tar.gz ftp://ftp.irisa.fr/local/texmex/corpus/gist.tar.gz
-tar -xvzf gist.tar.gz
--- setup/perf_data/download_miniset.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-wget -O miniset.zip https://cornell.box.com/shared/static/bibc6qz9p9abgkqmc4y2aryz64bm3idz.zip ./
-unzip ./miniset.zip
